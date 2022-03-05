6deadf17bdee0468e5e784eddcbf63d7828c1636
jhw2167 <jhw2167@columbia.edu>
2022-03-05 00:15:47 -0600

    added svg arrow with unique styling effects, looks great

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,15 +8,15 @@ function Header()  {
 
     return (
         <header>
-            <div className="logoImg d-inline header-div">
                 <div className='nested-banner-img'>
                     <img id="banner-img" src={banner}/>
                 </div>
-                <div className='banner-text'>Tracker</div>
-            </div>
-
-            <div className="logo-container">
+            
+            <div className="logo-title-container row">
+            <div className='col banner-text'>Tracker</div>
+            <div className="col logo-container">
                 <div className='logo-subcontainer' id='feet1'><img src={logo}/></div>
+            </div>
             </div>
         </header>
     )
--- src/resources/arrow.tsx
+++ src/resources/arrow.tsx
@@ -5,24 +5,41 @@ import React, { useState, useEffect } from 'react';
 //import { useCookies } from "react-cookie";
 //import { Modal, Button } from 'react-bootstrap';
 
+//svg
+import { ReactComponent as ArrowSvg } from './svg/arrow.svg';
+import ArrrowImg from './svg/arrow.svg';
+
 //CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/General.css'
 import * as CSS from 'csstype';
 
 interface ArrowProps {
-    styleClass?:string
+    height?: string;
+    width?: string;
     direction?: string
+
+    styleClass?:string;
     onClick?: Function;
 }
 
 
 function Arrow(props: ArrowProps) {
 
-    let symbol = (props.direction && props.direction=='left') ? '<' : '>';
+    const height = props.height ? props.height : '30px';
+    const width = props.width ? props.width : '40px';
+
+    let rotate = (props.direction && props.direction=='left') ?
+     'rotate(180)' : '';
     return(
-        <div className={'hyperlink-arrow ' + props.styleClass}
-        onClick={() => {if(props.onClick) props.onClick()}} ><p>{symbol}</p></div>
+        <div className={'hyperlink-arrow-div ' + props.styleClass + '-div'}
+        onClick={() => {if(props.onClick) props.onClick()}} >   
+        <ArrowSvg className={'svg-arrow ' + props.styleClass} 
+        fill='black'
+        height={height} width={width}
+        transform={rotate}
+        />
+        </div>
     );
 }
 
